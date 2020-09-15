271341f4a94eda97795efd466c0d7c394ec1e746
Watapax <watapax@gmail.com>
2020-09-15 18:22:21 -0300

    Clase 04

---
+++ Animacion2D/Assets/Clase 04/TriggerAnimation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("espada"))
+        {
+            GetComponent<Animator>().SetTrigger("golpe");
+        }
+    }
+
+}
