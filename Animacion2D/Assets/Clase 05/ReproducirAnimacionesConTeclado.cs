94c48d00a81d682687d44403d9450de6b9183858
Watapax <watapax@gmail.com>
2020-09-22 12:45:15 -0300

    clase 05

---
+++ Animacion2D/Assets/Clase 05/ReproducirAnimacionesConTeclado.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ReproducirAnimacionesConTeclado : MonoBehaviour
+{
+    Animator animator;
+    public string animacion1, animacion2, animacion3, animacion4;
+
+    private void Awake()
+    {
+        if(GetComponent<Animator>()!= null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
+    private void Update()
+    {
+        Animaciones();
+    }
+
+    void Animaciones()
+    {
+        if (animator == null) return;
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.Play(animacion1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animator.Play(animacion2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            animator.Play(animacion3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            animator.Play(animacion4);
+        }
+    }
+}
