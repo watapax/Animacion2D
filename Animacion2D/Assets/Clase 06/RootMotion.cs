dbf4ea5f11844d948929a71cbfb499e2aa2f97cd
Watapax <watapax@gmail.com>
2020-10-06 07:20:56 -0300

    clase 7

---
+++ Animacion2D/Assets/Clase 06/RootMotion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootMotion : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime ;
+        transform.Translate(Vector2.right * x);
+    }
+
+}
