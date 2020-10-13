4085eb10caffb20f3690845211a2592aee8628e3
Watapax <watapax@gmail.com>
2020-10-13 09:28:05 -0300

    clase 08

---
+++ Animacion2D/Assets/Clase 08/animaciones/AutoDestruirse.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class AutoDestruirse : MonoBehaviour
+{
+    public void Destrurise()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Animacion2D/Assets/Clase 08/animaciones/Impacto.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Impacto : MonoBehaviour
+{
+    public GameObject impactFxPrefab;
+    public Transform impactFxTransform;
+
+    public void MostrarImpacto()
+    {
+        Instantiate(impactFxPrefab, impactFxTransform.position, impactFxTransform.rotation);
+  
+    }
+
+
+    public void Sonido()
+    {
+        // reproducir un sonido;
+    }
+
+    public void Ejemplo()
+    {
+        Debug.Log("Aca el evento!!");
+    }
+}
