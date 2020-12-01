517666b94aec25f0a1e4f453bd510e65b1975e02
Watapax <watapax@gmail.com>
2020-12-01 12:58:36 -0300

    clase 15

---
+++ Animacion2D/Assets/Clase 15/ControlPersonaje.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPersonaje : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Detectar input de teclado (flechas)
+    // Mover al gameObject segun la direccion de la flecha
+    // almacenar en variable la direccion
+    public float velocidad;
+    float inputX;
+    Vector2 direccion;
+
+    private void Update()
+    {
+        // direccion sera igual al input Horizontal
+        inputX = Input.GetAxisRaw("Horizontal") * velocidad * Time.deltaTime;
+        direccion.x = inputX;
+
+        // el parametro caminando es verdadero cuando inputX no es cero
+        animator.SetBool("caminando", inputX != 0);
+
+        // si aprete el boton Salto, reproducir animacion ravenSalto
+        if(Input.GetButtonDown("Jump"))
+        {
+            // acceder al animator y darle play a la animacion
+            animator.Play("ravenSalto");
+        }
+
+
+        // si aprete el boton Fire1 ejecuta animacion accion1
+        // si aprete el boton Fire2 ejecuta animacion accion2
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            animator.Play("accion1");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            animator.Play("accion2");
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(direccion);
+    }
+
+
+}
---
+++ Animacion2D/Assets/Clase 15/PersonajeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeController : MonoBehaviour
+{
+    public Animator animator;
+    public float speed;
+    float inputHorizontal;
+    Vector2 direccion;
+    // detectar si aprete las flechas
+    // almacenar en un valor la direccion
+    // trasladar al gameObject segun la direccion
+
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        direccion.x = inputHorizontal;
+
+        animator.SetBool("caminando",  inputHorizontal != 0  );
+
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            // reproduce animacion accion1
+            animator.Play("accion1");
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // reproduce animacion accion1
+            animator.Play("accion2");
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(direccion);
+    }
+}
